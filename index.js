680d15d4b0d2b0de1535d82cad281847543ee5ce
Kornchaval <kornchaval@gmail.com>
2019-01-07 17:56:23 +0700

    add chToken

--- index.js
+++ index.js
@@ -4,10 +4,17 @@ const line = require('@line/bot-sdk');
 require('dotenv').config();
 
 const app = express();
-
+/*
 const config = {
     channelAccessToken: process.env.channelAccessToken,
-    channelSecret: process.env.channelSecret
+    channelSecret: process.env.channelSecret || 154e9bed54c5d0d70335e563d2089062
+
+};
+*/
+const config = {
+    channelAccessToken: 'gSUsaZB4IUt6h1TBQeZh8FPmR+/C3OVNcRNbC1JsVDj2OC6r7xUBqp6E9iG1HHtwWENJVsA80TJMt+/EPScTaosUOYFKCwCkxw1xb8BWdq4tYTb7MZHos+d1b/aBjDPeZLjKRb1O3jKMvFKekofjvgdB04t89/1O/w1cDnyilFU=',
+    channelSecret: '154e9bed54c5d0d70335e563d2089062'
+
 };
 
 const client = new line.Client(config);
@@ -18,7 +25,7 @@ app.post('/webhook', line.middleware(config), (req, res) => {
         .then((result) => res.json(result));
 });
 
-app.use('/hello', hello);
+//app.use('/hello', hello);
 
 let hello = (req, res, next)=> {
     res.setHeader('Content-Type', 'text/plain');
