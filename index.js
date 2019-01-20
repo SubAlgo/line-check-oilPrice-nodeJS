09b60bb808fc197264d1ecad3e684a0e64b40ec7
Kornchaval <kornchaval@gmail.com>
2019-01-21 02:56:45 +0700

    add handleText

--- index.js
+++ index.js
@@ -4,17 +4,10 @@ const line = require('@line/bot-sdk');
 require('dotenv').config();
 
 const app = express();
-/*
-const config = {
-    channelAccessToken: process.env.channelAccessToken,
-    channelSecret: process.env.channelSecret || 154e9bed54c5d0d70335e563d2089062
 
-};
-*/
 const config = {
     channelAccessToken: 'gSUsaZB4IUt6h1TBQeZh8FPmR+/C3OVNcRNbC1JsVDj2OC6r7xUBqp6E9iG1HHtwWENJVsA80TJMt+/EPScTaosUOYFKCwCkxw1xb8BWdq4tYTb7MZHos+d1b/aBjDPeZLjKRb1O3jKMvFKekofjvgdB04t89/1O/w1cDnyilFU=',
     channelSecret: '154e9bed54c5d0d70335e563d2089062'
-
 };
 
 const client = new line.Client(config);
@@ -25,15 +18,7 @@ app.post('/webhook', line.middleware(config), (req, res) => {
         .then((result) => res.json(result));
 });
 
-//app.use('/hello', hello);
-
-let hello = (req, res, next)=> {
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello World');
-}
-
 function handleEvent(event) {
-
     console.log(event);
     if (event.type === 'message' && event.message.type === 'text') {
         handleMessageEvent(event);
@@ -45,10 +30,23 @@ function handleEvent(event) {
 function handleMessageEvent(event) {
     var msg = {
         type: 'text',
-        text: 'Hi ' + event.message.text
+        text: handleText(event.message.text)
     };
 
     return client.replyMessage(event.replyToken, msg);
+}
+
+let handleText = (text) => {
+    let ms;
+    if(text = 'oil-now') {
+        ms = 'เดี๋ยวเช็คราคาน้ำมันให้นะ'
+    } else if (text == 'macbook') {
+        ms = 'อยากได้อยู่ T_T'
+    } else {
+        ms = 'Hi, ' + text
+    }
+
+    return ms;
 }
 
 app.set('port', (process.env.PORT || 5000));
