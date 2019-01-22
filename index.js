6b9afaf5eeb25e5466df550ad31d93527a5d5ab5
Kornchaval <kornchaval@gmail.com>
2019-01-22 08:07:17 +0700

    resultStr

--- index.js
+++ index.js
@@ -51,36 +51,35 @@ let checkoilPrice = () => {
         'MM' : 12,
         'YYYY' : 2018
     }
-    return args
-    //soap.createClient(url, function(err, client) {
-    //    let resultStr = 'test'
-    //    
-    //    client.GetOilPrice(args, function(err, result) {
-    //        
-    //        let MyStr = JSON.stringify(result)
-    //        let resultStr = ''
-    //        const $ = cheerio.load(MyStr);
-    //        $('DataAccess').each((i, el) => {
-    //            
-    //        
-    //            //check member in children element
-    //            const nChildren = $(el).children().length;
-    //            const children = $(el).children();
-    //            if(nChildren == 3) {
-    //                if(resultStr.trim() == '') {
-    //                    resultStr = $(children[1]).text()+ "\n--> " + $(children[2]).text()
-    //                } else {
-    //                    resultStr = resultStr + "\n" + $(children[1]).text()+ "\n--> " + $(children[2]).text()
-    //                }
-    //                resultStr = resultStr + "\n**********"
-    //        
-    //                console.log($(children[1]).text()+ " - " + $(children[2]).text())
-    //                console.log('*********')
-    //            }
-    //        })
-    //    });
-    //    return resultStr
-    //});
+    
+    soap.createClient(url, function(err, client) {
+        let resultStr = 'test'
+        
+        //client.GetOilPrice(args, function(err, result) {
+        //    
+        //    let MyStr = JSON.stringify(result)
+        //    let resultStr = ''
+        //    const $ = cheerio.load(MyStr);
+        //    $('DataAccess').each((i, el) => {
+        //    
+        //        //check member in children element
+        //        const nChildren = $(el).children().length;
+        //        const children = $(el).children();
+        //        if(nChildren == 3) {
+        //            if(resultStr.trim() == '') {
+        //                resultStr = $(children[1]).text()+ "\n--> " + $(children[2]).text()
+        //            } else {
+        //                resultStr = resultStr + "\n" + $(children[1]).text()+ "\n--> " + $(children[2]).text()
+        //            }
+        //            resultStr = resultStr + "\n**********"
+        //    
+        //            console.log($(children[1]).text()+ " - " + $(children[2]).text())
+        //            console.log('*********')
+        //        }
+        //    })
+        //});
+        return resultStr
+    });
     
 }
 
